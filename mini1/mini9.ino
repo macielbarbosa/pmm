dd662e64c5900c0030a90f6749c8e3a49f1cdadc
Maciel Barbosa <macielbarbosa@outlook.com>
2018-11-09 13:32:16 -0200

    Add files via upload

---
+++ mini1/mini9.ino
@@ -0,0 +1,37 @@
+#include<Servo.h>
+
+#define BOTAO 2
+#define RED_LED 7
+#define GREEN_LED 12
+#define SERVO 9
+
+Servo servo;
+int estado = 1;
+volatile int led = LOW;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BOTAO, INPUT_PULLUP);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(SERVO, OUTPUT);
+  servo.attach(SERVO);
+  servo.write(0);
+  attachInterrupt(digitalPinToInterrupt(2), blink, FALLING);
+}
+
+void loop() {
+  if (led) {
+    digitalWrite(RED_LED, HIGH);
+    delay(1000);
+    digitalWrite(RED_LED, LOW);
+    delay(1000);
+    led= !led;
+  }
+}
+
+void blink() {
+  led = !led;
+  digitalWrite(GREEN_LED, led);
+}
+
